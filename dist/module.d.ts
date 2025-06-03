997b81787ac94ed2ee805b59ce4082ae7c8a00ce
lubo <lubos.vlcek.work@gmail.com>
2025-06-03 19:43:33 +0200

    Working build with TypeScript v1.0.6

---
+++ dist/module.d.ts
@@ -0,0 +1,6 @@
+export interface ReportModuleOptions {
+    apiUrl?: string;
+    debug?: boolean;
+}
+declare const _default: import("@nuxt/schema").NuxtModule<ReportModuleOptions, ReportModuleOptions, false>;
+export default _default;
---
+++ dist/module.mjs
@@ -0,0 +1,12 @@
+import jiti from "file:///Users/lubo/workSpace/pomoc/report-module/node_modules/.pnpm/jiti@1.21.7/node_modules/jiti/lib/index.js";
+
+/** @type {import("/Users/lubo/workSpace/pomoc/report-module/src/module")} */
+const _module = jiti(null, {
+  "esmResolve": true,
+  "interopDefault": true,
+  "alias": {
+    "@goreslav/report-module": "/Users/lubo/workSpace/pomoc/report-module"
+  }
+})("/Users/lubo/workSpace/pomoc/report-module/src/module.ts");
+
+export default _module;
