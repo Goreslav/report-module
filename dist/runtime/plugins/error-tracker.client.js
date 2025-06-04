24f4afd23edd3b5a52c1e36bce8b86dd38ba067a
lubo <lubos.vlcek.work@gmail.com>
2025-06-04 21:34:28 +0200

    fix: remove plugin, use auto-init in composable v2.1.1

--- dist/runtime/plugins/error-tracker.client.js
+++ dist/runtime/plugins/error-tracker.client.js
@@ -1,13 +1,11 @@
+import { defineNuxtPlugin } from '#app'
 import { useCaptureUtils } from '../composables/useCaptureUtils'
 
 export default defineNuxtPlugin(() => {
-  // Spusti error tracking hneď pri načítaní stránky
   if (typeof window !== 'undefined') {
     const { startErrorTracking } = useCaptureUtils()
     startErrorTracking()
 
     console.log('🔍 Report Module: Error tracking started automatically on page load')
-
-    // Odstránené test errors - nebudeme automaticky generovať errory
   }
 })
